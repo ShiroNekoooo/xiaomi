1fb7d3e80dfbd06226bc4e0c01504ebfe2085bcd
轻度重症 <378406712@qq.com>
2019-09-03 22:41:01 +0800

    仿小米首页

---
+++ 小米/js/style.js
@@ -0,0 +1,11 @@
+
+  document.getElementsByClassName("short-brick-item").onmouseover=function(){
+      document.getElementsByClassName("review-wrapper").style.height="80px";
+      document.getElementsByClassName("review-wrapper").style.opacity="1";
+  }
+  document.getElementsByClassName("short-brick-item").onmouseover=function(){
+    document.getElementsByClassName("review-wrapper").style.height="0px";
+      document.getElementsByClassName("review-wrapper").style.opacity="0";
+  }
+
+
